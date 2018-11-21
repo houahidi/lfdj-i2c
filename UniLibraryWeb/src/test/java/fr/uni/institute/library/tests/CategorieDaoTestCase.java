2b33eff4a01382d8bf69a87f2c01fdf82c9d23f1
OUAHIDI <houahidi@uni-jnani-ho.local>
2018-11-21 13:30:36 +0100

    updates

--- UniLibraryWeb/src/test/java/fr/uni/institute/library/tests/CategorieDaoTestCase.java
+++ UniLibraryWeb/src/test/java/fr/uni/institute/library/tests/CategorieDaoTestCase.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.Collection;
 
 import org.junit.After;
@@ -18,6 +19,7 @@ public class CategorieDaoTestCase {
 
 	private CategoryDao categorieDao;
 	private int nombre;
+	Connection conn;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -25,9 +27,39 @@ public class CategorieDaoTestCase {
 		System.out.println("Initialisation des ressources");
 		
 		
-		String url = "jdbc:sqlite:library.db";
+		String url = "jdbc:sqlite:library-test.db";
 		Class.forName("org.sqlite.JDBC");
-		Connection conn = DriverManager.getConnection(url);
+		conn = DriverManager.getConnection(url);
+		
+		
+		 // SQL statement for creating a new table
+        
+        	
+        //String sql = "INSERT INTO t_category VALUES (2, NULL, 'Informatique')";
+        
+        //String sql = "drop table t_category";
+        
+        try {
+        	String sql = "CREATE TABLE t_category (\n" + 
+            		"  k_puid integer PRIMARY KEY AUTOINCREMENT ,\n" + 
+            		"  fk_puid_parent integer default NULL,\n" + 
+            		"  name varchar(255) NOT NULL\n" + 
+            		")";
+                Statement stmt = conn.createStatement();
+            // create a new table
+            stmt.execute(sql);
+            sql = "INSERT INTO t_category VALUES (1, NULL, 'Informatique')";
+            	stmt.execute(sql);
+            	sql = "INSERT INTO t_category VALUES (2, NULL, 'Sciences')";
+            	stmt.execute(sql);
+            conn.commit();
+            System.out.println("success");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+		
+		
+		
 		categorieDao = new CategoryDaoJdbc(conn);
 		nombre = 2;
 	}
@@ -35,6 +67,15 @@ public class CategorieDaoTestCase {
 	@After
 	public void tearDown() throws Exception {
 		System.out.println("liberation des ressources");
+		try {
+			String sql = "drop table t_category";
+			Statement stmt = conn.createStatement();
+            	stmt.execute(sql);
+            conn.commit();
+            System.out.println("success");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 	}
 
 	@Test()
